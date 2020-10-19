e11168b21b967b21489593533faa109ead0f2e7d
Franz Cordes-Eklund <franzcordeseklund@gmail.com>
2020-10-18 22:28:14 -0500

    fixed typo and gave buttons colors

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -43,8 +43,8 @@ function appendTasks(array) {
                 <tr  class="completed" data-id=${array[i].id}>
                 <td>${array[i].task_name}</td>
                 <td>Completed!</td>
-                <td><button type="submit" class="deleteBtn">Delete</button></td>
-                <td><button type="submit" class="completeBtn">Click when Completed</button></td>
+                <td><button type="submit" class="deleteBtn btn-warning">Delete</button></td>
+                <td><button type="submit" class="completeBtn btn-success">Click when Completed</button></td>
                 </tr>
             `)
         }
